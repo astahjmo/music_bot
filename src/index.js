35a42a59c2f819724d0a8a8e2c043a4e07535ff8
Johnatan <contato.johnatan373@gmail.com>
2022-01-17 18:10:13 -0300

    add express server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-const discord = require('discord.js')
 const { Intents } = require('discord.js')
 const { joinVoiceChannel } = require('@discordjs/voice')
+const discord = require('discord.js')
 
-const client = new discord.Client({ intents : [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.DIRECT_MESSAGES] , partials : ['CHANNEL', 'MESSAGE']})
+const client = new discord.Client({ intents : 
+    [Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.DIRECT_MESSAGES],
+    partials : ['CHANNEL', 'MESSAGE']})
 
+//Local imports
 const player = require("./play")
+
+require('dotenv').config()
+require('./server')
 
 client.on('ready', () => {
   client.guilds.fetch('829496232756903988').then((guild) => {
